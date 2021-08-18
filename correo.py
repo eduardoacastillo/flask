edde632e65f71b8d04b5802a3db764f90f15ce71
eduardo <eduardoacastillo222@gmail.com>
2021-08-17 21:22:25 -0500

    1er commit

---
+++ correo.py
@@ -0,0 +1,27 @@
+from flask import Flask
+from flask_mail import Mail
+from flask_mail import Message
+ 
+app = Flask(__name__)
+ 
+app.config['MAIL_SERVER']='smtp.gmail.com'
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USERNAME'] = 'crypsoft1@gmail.com'
+app.config['MAIL_PASSWORD'] = 'Sabbath_90210%'
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+ 
+mail = Mail(app)
+
+mail.init_app(app)
+
+@app.route("/")
+def index():
+  msg = Message("Hola Mundo", sender = 'crypsoft1@gmail.com', recipients = ['pysofting@gmail.com'])
+  msg.body = "Buenos dias, este correo está enviado desde una aplicación Flask "
+  mail.send(msg)
+
+  return "Mensaje enviado"
+ 
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ ejemplos5.py
@@ -0,0 +1,48 @@
+from flask import Flask, render_template, session, redirect, url_for
+from flask_wtf import FlaskForm
+
+from wtforms import (StringField, BooleanField, DateTimeField, RadioField, SelectField, TextField, TextAreaField, SubmitField)
+
+from wtforms.validators import DataRequired
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = 'miclavesecreta'
+
+class Formulario(FlaskForm):
+
+ nombre=StringField('Nombre',validators=[DataRequired()])
+ edad=BooleanField('Eres mayor de edad')
+ sexo=RadioField('Sexo:', choices=[('h', 'hombre'), ('m','mujer')])
+ color=SelectField('Color favorito:',
+    choices = [('r', 'rojo'), ('v', 'verde'), ('a', 'azul')])
+
+comentario = TextAreaField()
+boton = SubmitField('Enviar')
+
+@app.route('/informacion')
+def informacion():
+	return render_template('informacion.html')
+
+@app.route('/datos', methods= ['GET', 'POST'])
+def datos():
+    return render_template('datos.html')
+    miformulario = Formulario()
+
+    if miformulario.validate_on_submit():
+
+     session['nombre']= miformulario.nombre.data 
+     session['edad']= miformulario.edad.data
+     session['sexo']= miformulario.sexo.data
+     session['color']= miformulario.color.data
+     session['comentario']= miformulario.comentario.data
+
+    return redirect(url_for('informacion'))
+
+    return render_template('datos.html', formulario=miformulario)
+
+    if __name__== '__main__':
+     app.run(debug= True)
+
+
+
